bfa712123e16ccaa865b9eb2e4ac032572ca744e
qiandrewj <qiandrewj@gmail.com>
2023-11-16 19:32:58 -0500

    added db.py file and one test route

---
+++ backend/src/app.py
@@ -0,0 +1,45 @@
+import json
+
+from db import db
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+db_filename = "shelter_swipe.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+def success_response(data, code=200):
+    return json.dumps(data), code
+
+def failure_response(message, code=404):
+    return json.dumps({"error": message}), code
+
+# ROUTES -----------------------------------------
+@app.route("/")
+
+@app.route("/api/users/", methods = ["POST"])
+def create_user():
+    """
+    Endpoint for creating a user
+    """
+    body = json.loads(request.data)
+    if body.get("name") is None:
+        return failure_response("Missing information to create a new user", 400)
+    new_user = User(
+        name = body.get("name"),
+        species_pref = body.get("species_pref"),
+        age_pref = body.get("age_pref")
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return success_response(new_user.serialize(), 201)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
---
+++ backend/src/db.py
@@ -0,0 +1,148 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# Join tables -----------------------------
+
+user_pet = db.Table(
+    "user_table", db.Model.metadata,
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id")),
+    db.Column("pet_id", db.Integer, db.ForeignKey("pets.id"))
+)
+
+# Classes ---------------------------------
+class User(db.Model):
+    """
+    User model
+    """
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    name = db.Column(db.String, nullable = False)
+    # these might not work easily if we try to retrieve only preferred pets
+    species_pref = db.Column(db.String, nullable = True)
+    # int or string?
+    age_pref = db.Column(db.Integer, nullable = True)
+    pets = db.relationship("Pet", secondary = user_pet, back_populates="users")
+
+    def __init__(self, **kwargs):
+        """
+        Initialize a User object
+        """
+        self.name = kwargs.get("name", "")
+        self.species_pref = kwargs.get("species_pref", "")
+        self.species_pref = kwargs.get("age_pref", "")
+
+    def serialize(self):
+        """
+        Serializes a User object
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "species_pref": self.species_pref,
+            "age_pref": self.age_pref,
+            "pets": [p.simple_serialize() for p in self.pets]
+        }
+    
+    def simple_serialize(self):
+        """
+        Serializes a User object with only basic information
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "species_pref": self.species_pref,
+            "age_pref": self.age_pref
+        }
+
+class Pet(db.Model):
+    """
+    Pet model
+    """
+    __tablename__ = "pets"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    name = db.Column(db.String, nullable = False)
+    species = db.Column(db.String, nullable = False)
+    breed = db.Column(db.String, nullable = False)
+    age = db.Column(db.Integer, nullable = False)
+    likes = db.Column(db.String, nullable = True)
+    shelter_id = db.Column(db.Integer, db.ForeignKey("shelters.id", nullable = False))
+    users = db.relationship("User", secondary = user_pet, back_populates="pets")
+
+    def __init__(self, **kwargs):
+        """
+        Initialize a Pet object
+        """
+        self.name = kwargs.get("name", "")
+        self.species = kwargs.get("species", "")
+        self.breed = kwargs.get("breed", "")
+        self.age = kwargs.get("age", "")
+        self.likes = kwargs.get("likes","")
+        self.shelter_id = kwargs.get("shelter_id")
+
+    def serialize(self):
+        """
+        Serializes a Pet object
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "species": self.species,
+            "breed": self.breed,
+            "age": self.age,
+            "likes": self.likes,
+            "shelter": self.shelter_id,
+            "users": [u.simple_serialize() for u in self.users]
+        }
+    
+    def simple_serialize(self):
+        """
+        Serializes a Pet object with only basic information
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "species": self.species,
+            "breed": self.breed,
+            "age": self.age,
+            "likes": self.likes,
+            "shelter": self.shelter_id
+        }
+
+class Shelter(db.Model):
+    """
+    Shelter model
+    """
+    __tablename__ = "shelters"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    name = db.Column(db.String, nullable = False)
+    location = db.Column(db.String, nullable = False)
+    pets = db.relationship("Pet", cascade = "delete")
+
+    def __init__(self, **kwargs):
+        """
+        Initialize a Shelter object
+        """
+        self.name = kwargs.get("name", "")
+        self.location = kwargs.get("location", "")
+
+    def serialize(self):
+        """
+        Serializes a Shelter object
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "location": self.location,
+            "pets": [p.simple_serialize() for p in self.pets]
+        }
+    
+    def simple_serialize(self):
+        """
+        Serializes a Shelter object with only basic information
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "location": self.location,
+        }
