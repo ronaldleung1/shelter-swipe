ec2b1b723907d094fa180f39cf0f1994b3216fd9
Cassidy <chx4@cornell.edu>
2023-11-18 20:55:34 -0500

    setup folders

---
+++ ShelterSwipe/ShelterSwipe/Views/SavedView.swift
@@ -0,0 +1,18 @@
+//
+//  SavedView.swift
+//  ShelterSwipe
+//
+//  Created by Cassidy Xu on 11/18/23.
+//
+
+import SwiftUI
+
+struct SavedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavedView()
+}
