73606301e1eb0531137ac3da721e4ab9eec1df96
Cassidy <chx4@cornell.edu>
2023-11-18 20:45:46 -0500

    frontend framework

---
+++ ShelterSwipe/ShelterSwipe/ShelterSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShelterSwipeApp.swift
+//  ShelterSwipe
+//
+//  Created by Cassidy Xu on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ShelterSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
